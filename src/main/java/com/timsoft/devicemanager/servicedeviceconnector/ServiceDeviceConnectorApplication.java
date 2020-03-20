251ee834764f384cfd380e8b41aca16f7ff61ac7
coder-supratim <supratim.arya@gmail.com>
2020-03-20 00:23:03 -0400

    Initial commit

---
+++ src/main/java/com/timsoft/devicemanager/servicedeviceconnector/ServiceDeviceConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.timsoft.devicemanager.servicedeviceconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeviceConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDeviceConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timsoft/devicemanager/servicedeviceconnector/ServiceDeviceConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timsoft.devicemanager.servicedeviceconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDeviceConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
