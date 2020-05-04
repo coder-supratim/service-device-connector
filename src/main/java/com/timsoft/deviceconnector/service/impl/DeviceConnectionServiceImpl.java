df7ca4512cb2d1944a0841fd78303b90c615d482
coder-supratim <supratim.arya@gmail.com>
2020-05-04 01:48:35 -0400

    Service modified

--- src/main/java/com/timsoft/deviceconnector/service/impl/DeviceConnectionServiceImpl.java
+++ src/main/java/com/timsoft/deviceconnector/service/impl/DeviceConnectionServiceImpl.java
@@ -3,15 +3,10 @@ package com.timsoft.deviceconnector.service.impl;
 import com.timsoft.deviceconnector.config.DeviceConnectorServiceConfig;
 import com.timsoft.deviceconnector.model.DeviceConnection;
 import com.timsoft.deviceconnector.service.DeviceConnectionService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,16 +18,25 @@ public class DeviceConnectionServiceImpl implements DeviceConnectionService {
     private DeviceConnectorServiceConfig deviceConnectorServiceConfig;
     @Value("${registration.service.endpoint}")
     private String deviceRegistrationEndpoint;
-    //= "http://localhost:8081/device-registrar/v1/isregistered/y889id";
 
     @Override
-    public DeviceConnection getConnection(String deviceName) {
+    public DeviceConnection getConnection(String deviceId) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        HttpEntity httpEntity = new HttpEntity<>(httpHeaders);
         ResponseEntity<DeviceRegistration> deviceRegistrationResponse = deviceConnectorServiceConfig.getRestTemplate()
-                .exchange(deviceRegistrationEndpoint, HttpMethod.GET, httpEntity, DeviceRegistration.class, deviceName);
-        log.info("Device registration service invoked successfully");
-        return new DeviceConnection();
+                .exchange(deviceRegistrationEndpoint, HttpMethod.GET, httpEntity, DeviceRegistration.class, deviceId);
+        log.info("Device registration service invoked to verify active device for device id {}", deviceId);
+        return mockResponse(deviceId);
+    }
+    private DeviceConnection mockResponse(String deviceId){
+        DeviceConnection mockConnection = new DeviceConnection();
+        mockConnection.setDeviceId(deviceId);
+        mockConnection.setDeviceName("Tim's Ring video doorbell");
+        mockConnection.setProvider("Ring");
+        mockConnection.setConnectionProtocol("wifi");
+
+
+
     }
 }
